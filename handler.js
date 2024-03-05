8fc62c62d2f4db40a50bc7b041d8caff0bfb3ebb
marcos <marcos.barboza@jbtec.com.br>
2024-03-05 12:18:53 -0300

    feat: range time

--- handler.js
+++ handler.js
@@ -35,6 +35,62 @@ const ffmpeg = async (inputFile, outputFile) =>
         });
     });
 
+const secondsToHms = (d) => {
+    const convert = Number(d);
+    const h = Math.floor(convert / 3600);
+    const m = Math.floor((convert % 3600) / 60);
+    const s = Math.floor((convert % 3600) % 60);
+
+    const hDisplay = h > 0 ? h + (h === 1 ? ':' : ':') : '';
+    const mDisplay = m > 0 ? m + (m === 1 ? ':' : ':') : '';
+    const sDisplay = s > 0 ? s : '';
+    return hDisplay + mDisplay + sDisplay;
+};
+
+const ffmpegClip = async (inputFile, outputFile, rangeTime) =>
+    new Promise((resolve, reject) => {
+        const command = ['-i', inputFile, '-c', 'copy', outputFile];
+
+        if (rangeTime) {
+            const { start, end } = rangeTime;
+            console.log('start:', start);
+            console.log('end:', end);
+            const duration = parseInt(end, 10) - parseInt(start, 10);
+            command.unshift(secondsToHms(duration));
+            command.unshift('-t');
+            command.unshift(secondsToHms(start));
+            command.unshift('-ss');
+        }
+
+        console.log('command:', command.join(' '));
+
+        const process = spawn('/opt/ffmpeg/ffmpeg', command);
+
+        process.stderr.on('data', (data) => {
+            console.log(data.toString());
+        });
+
+        process.stdout.on('data', (data) => {
+            console.error(data.toString());
+        });
+
+        process.on('error', (error) => {
+            console.error('Error from process:', error);
+        });
+
+        process.stderr.on('error', (error) => {
+            console.error('Error from stderr:', error);
+        });
+
+        process.on('close', (code) => {
+            if (code === 0) {
+                resolve();
+            } else {
+                reject(new Error('ffmpeg failed'));
+            }
+        });
+    });
+
 const downloadFromS3 = async ({ file, bucket, region }) => {
     const s3 = new s3client.S3Client({ region });
     const data = await s3.send(
@@ -47,6 +103,8 @@ const downloadFromS3 = async ({ file, bucket, region }) => {
     const parsedFileName = parseFileName(endFileName);
     await fs.mkdir(parsedFileName.dir, { recursive: true });
     await fs.writeFile(join('/', 'tmp', file), data.Body);
+
+    return data.Metadata;
 };
 
 const uploadToS3 = async ({ file, key, bucket, region }) => {
@@ -61,22 +119,77 @@ const uploadToS3 = async ({ file, key, bucket, region }) => {
     );
 };
 
-const generateThumb = async ({ filename, bucket, region }) => {
+const generateClipAndUpload = async ({
+    filename,
+    bucket,
+    region,
+    rangeTime,
+}) => {
     const parsedFileName = parseFileName(filename);
-    const thumbFilename = `${parsedFileName.name}_thumb.jpg`;
-    await downloadFromS3({
-        file: filename,
-        bucket,
-        region,
-    });
-    await ffmpeg(join('/', 'tmp', filename), join('/', 'tmp', thumbFilename));
+    const thumbFilename = `${parsedFileName.name}_thumb.mp4`;
+
     const endFileName = join(parsedFileName.dir, thumbFilename);
+
+    console.log('endfilename:', endFileName);
+    console.log('rangeTime:', rangeTime);
+
+    await ffmpegClip(
+        join('/', 'tmp', filename),
+        join('/', 'tmp', thumbFilename),
+        rangeTime
+    );
+
+    console.log('clip generated:', join('/', 'tmp', thumbFilename));
+
+    console.log('thumbFilename:', thumbFilename);
+    console.log('endFileName:', endFileName);
+
     await uploadToS3({
         file: thumbFilename,
         key: endFileName,
         bucket,
         region,
     });
+};
+
+const generateThumb = async ({ filename, bucket, region }) => {
+    console.log('filename:', filename);
+    const parsedFileName = parseFileName(filename);
+    console.log('parsedFileName:', parsedFileName);
+
+    if (parsedFileName.name.endsWith('_thumb')) {
+        console.log('File has already been processed, skipping');
+        return;
+    }
+
+    const metadata = await downloadFromS3({
+        file: filename,
+        bucket,
+        region,
+    });
+
+    console.log('metadata:', metadata);
+
+    if (metadata && metadata.rangetimestart && metadata.rangetimeend) {
+        await generateClipAndUpload({
+            filename,
+            bucket,
+            region,
+            rangeTime: {
+                start: metadata.rangetimestart,
+                end: metadata.rangetimeend,
+            },
+        });
+    }
+
+    // await ffmpeg(join('/', 'tmp', filename), join('/', 'tmp', thumbFilename));
+    // const endFileName = join(parsedFileName.dir, thumbFilename);
+    // await uploadToS3({
+    //     file: thumbFilename,
+    //     key: endFileName,
+    //     bucket,
+    //     region,
+    // });
 };
 
 module.exports.postprocess = async (event) => {
