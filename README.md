bced9159338c7c359b9f412b4753e6a7ae968f11
marcos <marcos.barboza@jbtec.com.br>
2024-03-12 18:56:45 -0300

    feat: compress video

--- README.md
+++ README.md
@@ -7,11 +7,11 @@ A simple service for generate a thumb from a video
 ### Production
 
 ```bash
-sls deploy -s production
+sls deploy -s production --param="NOTIFY_API_URL=https://studio-api.vitruveo.xyz"
 ```
 
 ### Quality Assuarance
 
 ```bash
-sls deploy -s qa
+sls deploy -s qa --param="NOTIFY_API_URL=https://studio-api.vtru.dev"
 ```
--- handler.js
+++ handler.js
@@ -1,39 +1,26 @@
+/* eslint-disable no-return-assign */
+/* eslint-disable no-await-in-loop */
 /* eslint-disable no-console */
+const axios = require('axios');
 const { spawn } = require('child_process');
 const { parse: parseFileName, join } = require('path');
 const s3client = require('@aws-sdk/client-s3');
 const fs = require('fs/promises');
 
-const ffmpeg = async (inputFile, outputFile) =>
-    new Promise((resolve, reject) => {
-        const process = spawn('/opt/ffmpeg/ffmpeg', [
-            '-i',
-            inputFile,
-            '-vf',
-            'thumbnail',
-            '-qscale:v',
-            '3',
-            '-frames:v',
-            '1',
-            outputFile,
-        ]);
+const notify = async ({ filename, size }) => {
+    const url = `${process.env.NOTIFY_API_URL}/assets/notify/file`;
+    const data = { filename, size };
 
-        process.stderr.on('data', (data) => {
-            console.log(data.toString());
-        });
+    console.log('url:', url);
 
-        process.stdout.on('data', (data) => {
-            console.error(data.toString());
-        });
-
-        process.on('close', (code) => {
-            if (code === 0) {
-                resolve();
-            } else {
-                reject(new Error('ffmpeg failed'));
-            }
-        });
-    });
+    try {
+        const response = await axios.put(url, data);
+        console.log(`Status: ${response.status}`);
+        console.log('Body: ', response.data);
+    } catch (error) {
+        console.error(`Error: ${error}`);
+    }
+};
 
 const secondsToHms = (d) => {
     const convert = Number(d);
@@ -47,48 +34,97 @@ const secondsToHms = (d) => {
     return hDisplay + mDisplay + sDisplay;
 };
 
-const ffmpegClip = async (inputFile, outputFile, rangeTime) =>
+const getVideoDurationInSeconds = (filePath) =>
     new Promise((resolve, reject) => {
-        const command = ['-i', inputFile, '-c', 'copy', outputFile];
+        const command = spawn('/opt/ffmpeg/ffprobe', [
+            '-v',
+            'error',
+            '-show_entries',
+            'format=duration',
+            '-of',
+            'default=noprint_wrappers=1:nokey=1',
+            filePath,
+        ]);
 
-        if (rangeTime) {
+        let output = '';
+
+        command.stdout.on('data', (chunk) => (output += chunk));
+        command.on('close', (code) => {
+            if (code !== 0) {
+                reject(new Error(`ffprobe exited with code ${code}`));
+            } else {
+                resolve(parseFloat(output));
+            }
+        });
+    });
+
+const ffmpegTransformVideo = async (
+    inputFile,
+    outputFile,
+    rangeTime,
+    width,
+    height,
+    bitrate
+) =>
+    new Promise((resolve, reject) => {
+        const command = ['-y', '-i', inputFile];
+        let change = false;
+
+        if (width && height) {
+            command.push('-vf', `scale=${width}:${height}`);
+            change = true;
+        }
+
+        if (bitrate) {
+            command.push('-b:v', `${bitrate}k`);
+            change = true;
+        }
+
+        command.push(outputFile);
+
+        if (rangeTime?.start && rangeTime?.end) {
             const { start, end } = rangeTime;
             console.log('start:', start);
             console.log('end:', end);
             const duration = parseInt(end, 10) - parseInt(start, 10);
             command.unshift(secondsToHms(duration));
             command.unshift('-t');
-            command.unshift(secondsToHms(start));
+            command.unshift(secondsToHms(start === '0' ? '1' : start));
             command.unshift('-ss');
+            change = true;
         }
 
-        console.log('command:', command.join(' '));
+        if (!change) {
+            resolve(change);
+        } else {
+            console.log('command:', command.join(' '));
 
-        const process = spawn('/opt/ffmpeg/ffmpeg', command);
+            const process = spawn('/opt/ffmpeg/ffmpeg', command);
 
-        process.stderr.on('data', (data) => {
-            console.log(data.toString());
-        });
+            process.stderr.on('data', (data) => {
+                console.log(data.toString());
+            });
 
-        process.stdout.on('data', (data) => {
-            console.error(data.toString());
-        });
+            process.stdout.on('data', (data) => {
+                console.error(data.toString());
+            });
 
-        process.on('error', (error) => {
-            console.error('Error from process:', error);
-        });
+            process.on('error', (error) => {
+                console.error('Error from process:', error);
+            });
 
-        process.stderr.on('error', (error) => {
-            console.error('Error from stderr:', error);
-        });
+            process.stderr.on('error', (error) => {
+                console.error('Error from stderr:', error);
+            });
 
-        process.on('close', (code) => {
-            if (code === 0) {
-                resolve();
-            } else {
-                reject(new Error('ffmpeg failed'));
-            }
-        });
+            process.on('close', (code) => {
+                if (code === 0) {
+                    resolve(change);
+                } else {
+                    reject(new Error('ffmpeg failed'));
+                }
+            });
+        }
     });
 
 const downloadFromS3 = async ({ file, bucket, region }) => {
@@ -119,37 +155,89 @@ const uploadToS3 = async ({ file, key, bucket, region }) => {
     );
 };
 
-const generateClipAndUpload = async ({
+const generateClipAndReplaceOriginal = async ({
     filename,
     bucket,
     region,
     rangeTime,
+    maxSize,
+    width,
+    height,
 }) => {
     const parsedFileName = parseFileName(filename);
-    const thumbFilename = `${parsedFileName.name}_thumb.mp4`;
 
-    const endFileName = join(parsedFileName.dir, thumbFilename);
-
-    console.log('endfilename:', endFileName);
+    console.log('filename:', filename);
     console.log('rangeTime:', rangeTime);
 
-    await ffmpegClip(
-        join('/', 'tmp', filename),
-        join('/', 'tmp', thumbFilename),
-        rangeTime
+    const tempFilename = `${parsedFileName.name}_temp.mp4`;
+
+    console.log('tempFilename:', tempFilename);
+
+    console.log('width:', width, 'height:', height);
+
+    const filePath = join('/', 'tmp', filename);
+
+    let stats = await fs.stat(join('/', 'tmp', filename));
+    let bitrate = null;
+    const fileSizeInBytes = stats.size;
+    const fileSizeInMegabytes = fileSizeInBytes / (1024 * 1024);
+
+    console.log(
+        `meu video: ${fileSizeInMegabytes} é maior que ${maxSize}?`,
+        fileSizeInMegabytes > maxSize
     );
 
-    console.log('clip generated:', join('/', 'tmp', thumbFilename));
+    if (fileSizeInMegabytes > maxSize) {
+        const fileSizeInKb = Number.parseInt(maxSize, 10) * 8192;
+        if (rangeTime?.start && rangeTime?.end) {
+            bitrate =
+                fileSizeInKb /
+                (parseInt(rangeTime.end, 10) - parseInt(rangeTime.start, 10));
+        } else {
+            const durationInSeconds = await getVideoDurationInSeconds(filePath);
+            bitrate = fileSizeInKb / durationInSeconds;
+        }
+    }
 
-    console.log('thumbFilename:', thumbFilename);
-    console.log('endFileName:', endFileName);
+    console.log('bitrate:', bitrate);
+
+    const res = await ffmpegTransformVideo(
+        filePath,
+        join('/', 'tmp', tempFilename),
+        rangeTime,
+        width,
+        height,
+        bitrate
+    );
+
+    console.log('passei aqui:', res);
+
+    if (!res) {
+        await notify({ filename, size: stats.size });
+        return;
+    }
+
+    await fs.rename(join('/', 'tmp', tempFilename), join('/', 'tmp', filename));
+
+    stats = await fs.stat(join('/', 'tmp', filename));
+
+    console.log(
+        'filename:',
+        filename,
+        'key:',
+        join(parsedFileName.dir, filename),
+        'size:',
+        stats.size
+    );
 
     await uploadToS3({
-        file: thumbFilename,
-        key: endFileName,
+        file: filename,
+        key: filename,
         bucket,
         region,
     });
+
+    await notify({ filename, size: stats.size });
 };
 
 const generateThumb = async ({ filename, bucket, region }) => {
@@ -170,30 +258,24 @@ const generateThumb = async ({ filename, bucket, region }) => {
 
     console.log('metadata:', metadata);
 
-    if (metadata && metadata.rangetimestart && metadata.rangetimeend) {
-        await generateClipAndUpload({
+    if (metadata && Object.values(metadata)?.length > 0) {
+        await generateClipAndReplaceOriginal({
             filename,
             bucket,
             region,
+            width: metadata.width,
+            height: metadata.height,
+            maxSize: metadata.maxsize,
             rangeTime: {
                 start: metadata.rangetimestart,
                 end: metadata.rangetimeend,
             },
         });
     }
-
-    // await ffmpeg(join('/', 'tmp', filename), join('/', 'tmp', thumbFilename));
-    // const endFileName = join(parsedFileName.dir, thumbFilename);
-    // await uploadToS3({
-    //     file: thumbFilename,
-    //     key: endFileName,
-    //     bucket,
-    //     region,
-    // });
 };
 
 module.exports.postprocess = async (event) => {
-    await Promise.all(
+    await Promise.allSettled(
         event.Records.map((record) =>
             generateThumb({
                 filename: record.s3.object.key,
